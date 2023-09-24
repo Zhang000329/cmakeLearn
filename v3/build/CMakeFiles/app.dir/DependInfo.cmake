ceecb159a4382877db13ccf7b0997c6f1aa10ccc
Kevin <zhangyujun000329@163.com>
2023-09-24 22:54:46 +0800

    v3

---
+++ v3/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/zhangyujun/vscode/ptr/cmakeLearn/v3/add.c" "CMakeFiles/app.dir/add.c.o" "gcc" "CMakeFiles/app.dir/add.c.o.d"
+  "/Users/zhangyujun/vscode/ptr/cmakeLearn/v3/div.c" "CMakeFiles/app.dir/div.c.o" "gcc" "CMakeFiles/app.dir/div.c.o.d"
+  "/Users/zhangyujun/vscode/ptr/cmakeLearn/v3/main.c" "CMakeFiles/app.dir/main.c.o" "gcc" "CMakeFiles/app.dir/main.c.o.d"
+  "/Users/zhangyujun/vscode/ptr/cmakeLearn/v3/mult.c" "CMakeFiles/app.dir/mult.c.o" "gcc" "CMakeFiles/app.dir/mult.c.o.d"
+  "/Users/zhangyujun/vscode/ptr/cmakeLearn/v3/sub.c" "CMakeFiles/app.dir/sub.c.o" "gcc" "CMakeFiles/app.dir/sub.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
