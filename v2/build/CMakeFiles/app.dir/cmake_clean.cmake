ca6179e954d06b5d3c6e2be4732e5d06645a8bcb
Kevin <zhangyujun000329@163.com>
2023-09-24 17:15:09 +0800

    first commit

---
+++ v2/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/Users/zhangyujun/vscode/ptr/cmakeLearn/v2/output/app"
+  "/Users/zhangyujun/vscode/ptr/cmakeLearn/v2/output/app.pdb"
+  "CMakeFiles/app.dir/add.c.o"
+  "CMakeFiles/app.dir/add.c.o.d"
+  "CMakeFiles/app.dir/div.c.o"
+  "CMakeFiles/app.dir/div.c.o.d"
+  "CMakeFiles/app.dir/main.c.o"
+  "CMakeFiles/app.dir/main.c.o.d"
+  "CMakeFiles/app.dir/mult.c.o"
+  "CMakeFiles/app.dir/mult.c.o.d"
+  "CMakeFiles/app.dir/sub.c.o"
+  "CMakeFiles/app.dir/sub.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
