c311743e678e9a7cdacc0c20e1ea8b29da0b47dc
root <root@kevin.localdomain>
2023-09-25 11:33:00 +0800

    v5

---
+++ v5/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/add.c.o"
+  "CMakeFiles/app.dir/src/div.c.o"
+  "CMakeFiles/app.dir/src/main.c.o"
+  "CMakeFiles/app.dir/src/mult.c.o"
+  "CMakeFiles/app.dir/src/sub.c.o"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ v5/build/CMakeFiles/calc.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../lib/libcalc.pdb"
+  "../lib/libcalc.so"
+  "CMakeFiles/calc.dir/src/add.c.o"
+  "CMakeFiles/calc.dir/src/div.c.o"
+  "CMakeFiles/calc.dir/src/main.c.o"
+  "CMakeFiles/calc.dir/src/mult.c.o"
+  "CMakeFiles/calc.dir/src/sub.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
